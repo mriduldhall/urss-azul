41406e987f87ee98f97c34e0b0df768a388eac5b
Mridul Dhall <mriduldhall1@gmail.com>
2026-08-20 18:37:28 +0100

    Check victory in tic tac toe

--- src/environments/tic_tac_toe/game.py
+++ src/environments/tic_tac_toe/game.py
@@ -23,6 +23,17 @@ class Game:
         self.current_player = self.player_two_marker if self.current_player is self.player_one_marker else self.player_one_marker
         return True
 
+    def check_victory(self):
+        possible_combinations = [
+            (0, 1, 2), (3, 4, 5), (6, 7, 8),
+            (0, 3, 6), (1, 4, 7), (2, 5, 8),
+            (0, 4, 8), (2, 4, 6)
+        ]
+        for combination in possible_combinations:
+            if self.board[combination[0]] == self.board[combination[1]] == self.board[combination[2]] != Marker.EMPTY:
+                return self.board[combination[0]]
+        return None
+
     def display_board(self):
         return f"{self.board[0].value} | {self.board[1].value} | {self.board[2].value}\n" \
                f"---------\n" \
