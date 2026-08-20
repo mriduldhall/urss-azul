6a8c195d6053ee8ded69d3b545acbb8a3c293403
Mridul Dhall <mriduldhall1@gmail.com>
2026-08-20 23:09:35 +0100

    Build main to run two player tic tac toe

--- src/main.py
+++ src/main.py
@@ -1,2 +1,11 @@
+from agents.human import HumanAgent
+from environments.tic_tac_toe.game import Game
+from runner import Runner
+
+
 if __name__ == '__main__':
-    pass
+    game = Game()
+    player_one_agent = HumanAgent(game)
+    player_two_agent = HumanAgent(game)
+    runner = Runner(game, player_one_agent, player_two_agent)
+    runner.run_game()
