980026b5d007113969cfcd38c4559d10ca9b28cd
Mridul Dhall <mriduldhall1@gmail.com>
2026-08-20 18:34:20 +0100

    Allow player to make move

--- src/environments/tic_tac_toe/game.py
+++ src/environments/tic_tac_toe/game.py
@@ -16,6 +16,13 @@ class Game:
     def get_legal_actions(self):
         return [position for position, marker in enumerate(self.board) if marker is Marker.EMPTY]
 
+    def make_move(self, position):
+        if position not in self.get_legal_actions():
+            return None
+        self.board[position] = self.current_player
+        self.current_player = self.player_two_marker if self.current_player is self.player_one_marker else self.player_one_marker
+        return True
+
     def display_board(self):
         return f"{self.board[0].value} | {self.board[1].value} | {self.board[2].value}\n" \
                f"---------\n" \
