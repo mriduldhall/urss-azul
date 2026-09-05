8dfb74b1a067eebea076b656f0e5d695d074faa3
Mridul Dhall <mriduldhall1@gmail.com>
2026-09-05 16:41:12 +0100

    Build Azul floor line class

---
+++ src/environments/azul/floor_line.py
@@ -0,0 +1,26 @@
+from tiles import Tiles
+
+class FloorLine:
+    PENALTIES = (1, 1, 2, 2, 2, 3, 3)
+
+    def __init__(self):
+        self.tiles = []
+
+    def first_player_tile(self):
+        if Tiles.STARTING in self.tiles:
+            return True
+        return False
+
+    def add_tiles(self, tile, number):
+        self.tiles.extend([tile] * number)
+
+    def points_lost(self):
+        total_penalty = 0
+        for i in range(min(len(self.tiles), len(self.PENALTIES))):
+            total_penalty += self.PENALTIES[i]
+        return total_penalty
+
+    def clear(self):
+        removed_tiles = self.tiles.copy()
+        self.tiles.clear()
+        return removed_tiles
