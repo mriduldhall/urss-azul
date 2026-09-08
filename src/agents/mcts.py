fbaa10230a447ae679000bb0886dde9d3a7db193
Mridul Dhall <mriduldhall1@gmail.com>
2026-09-08 20:49:22 +0100

    Allow seeded RNG for MCTS agent

--- src/agents/mcts.py
+++ src/agents/mcts.py
@@ -1,5 +1,5 @@
+from random import Random
 from math import log, sqrt
-from random import choice
 
 
 class StateNode:
@@ -11,10 +11,10 @@ class StateNode:
         self.visits = 0
         self.reward = 0
 
-    def expand_node(self, legal_moves):
+    def expand_node(self, legal_moves, rng):
         tried_moves = [child.move for child in self.children]
         untried_moves = [move for move in legal_moves if move not in tried_moves]
-        move = choice(untried_moves)
+        move = rng.choice(untried_moves)
         child_state = self.state.clone()
         child_state.make_move(move)
         child_node = StateNode(child_state, move, self)
@@ -39,8 +39,9 @@ class StateNode:
 
 
 class MonteCarloTreeSearchAgent:
-    def __init__(self, game, simulations=1000, exploration_constant=sqrt(2)):
+    def __init__(self, game, simulations=1000, exploration_constant=sqrt(2), rng=None):
         self.game = game
+        self.rng = rng if rng is not None else Random()
         self.root = StateNode(self.game.clone(), None, None)
         self.simulations = simulations
         self.exploration_constant = exploration_constant
@@ -63,20 +64,20 @@ class MonteCarloTreeSearchAgent:
                 return current_node
             legal_moves = current_node.state.get_legal_actions()
             if len(current_node.children) < len(legal_moves):
-                return current_node.expand_node(legal_moves)
+                return current_node.expand_node(legal_moves, self.rng)
             else:
                 current_node = self.select_child(current_node)
 
     def run_simulation(self, state):
         cloned_state = state.clone()
         while not cloned_state.check_end():
-            move = choice(cloned_state.get_legal_actions())
+            move = self.rng.choice(cloned_state.get_legal_actions())
             cloned_state.make_move(move)
         winner = cloned_state.check_victory()
         if winner is None:
             victory = None
         else:
-            victory = winner == self.game.current_player
+            victory = winner == self.game.current_player.value
         return victory
 
     @staticmethod
