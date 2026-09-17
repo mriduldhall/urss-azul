16cd04ed8bf1c19409f7efd477fcb825a3125f13
Mridul Dhall <mriduldhall1@gmail.com>
2026-09-17 16:16:27 +0100

    Refactor mcts agents

--- src/agents/mcts/mcts_chance.py
+++ src/agents/mcts/mcts_chance.py
@@ -1,6 +1,7 @@
 from random import Random
 from math import log, sqrt
 
+#noinspection DuplicatedCode
 class StateNode:
     def __init__(self, state, move, parent):
         self.state = state
@@ -14,8 +15,10 @@ class StateNode:
         tried_moves = [child.move for child in self.children]
         untried_moves = [move for move in legal_moves if move not in tried_moves]
         move = rng.choice(untried_moves)
+
         child_state = self.state.clone()
         child_state.apply_deterministic_move(move)
+
         child_node = StateNode(child_state, move, self)
         self.children.append(child_node)
         return child_node
@@ -23,20 +26,25 @@ class StateNode:
     def calculate_uct(self, exploration_constant, is_maximising):
         if self.visits == 0:
             return float('inf')
+
         win_rate = self.reward / self.visits
         if not is_maximising:
             win_rate = 1 - win_rate
+
         exploration_term = exploration_constant * sqrt(log(self.parent.visits) / self.visits)
         return win_rate + exploration_term
 
     def update(self, win):
         self.visits += 1
+
         if win:
             self.reward += 1
+
         if win is None:
             self.reward += 0.5
 
 
+#noinspection DuplicatedCode
 class MonteCarloTreeSearchChanceAgent:
     def __init__(self, game, simulations=1000, exploration_constant=sqrt(2), rng=None, chance_policy=None):
         self.game = game
@@ -50,11 +58,13 @@ class MonteCarloTreeSearchChanceAgent:
         best_uct = -float('inf')
         best_child = None
         is_maximising = current_node.state.current_player.value == self.game.current_player.value
+
         for child in current_node.children:
             uct_value = child.calculate_uct(self.exploration_constant, is_maximising)
             if uct_value > best_uct:
                 best_uct = uct_value
                 best_child = child
+
         return best_child
 
     def find_node(self):
@@ -62,11 +72,12 @@ class MonteCarloTreeSearchChanceAgent:
         while True:
             if current_node.state.check_end() or current_node.state.chance_node_required():
                 return current_node
+
             legal_moves = current_node.state.get_legal_actions()
             if len(current_node.children) < len(legal_moves):
                 return current_node.expand_node(legal_moves, self.rng)
-            else:
-                current_node = self.select_child(current_node)
+
+            current_node = self.select_child(current_node)
 
     def run_simulation(self, state):
         cloned_state = state.clone()
@@ -79,13 +90,16 @@ class MonteCarloTreeSearchChanceAgent:
                     cloned_state = self.chance_policy.sample_outcome(cloned_state)
                     first_chance_node = False
                 cloned_state.resolve_chance()
+
             move = self.rng.choice(cloned_state.get_legal_actions())
             cloned_state.apply_deterministic_move(move)
+
         winner = cloned_state.check_victory()
         if winner is None:
             victory = None
         else:
             victory = winner == self.game.current_player.value
+
         return victory
 
     @staticmethod
