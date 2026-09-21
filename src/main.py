2014c18245f19d5bccc1220c652c1b7eb125dd92
Mridul Dhall <mriduldhall1@gmail.com>
2026-09-21 14:47:40 +0100

    Restructure policies into respective agents

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
 from random import Random
+
+from runner import Runner
+
+from environments.azul.game import Game as AzulGame
+from environments.tic_tac_toe.game import Game as TicTacToeGame
+
+from interfaces.azul_console import AzulInputHandler
+from interfaces.tic_tac_toe_console import TicTacToeInputHandler
+
+from heuristics.azul_net_expected import AzulNetExpectedHeuristic
+
 from agents.human import HumanAgent
 from agents.random import RandomAgent
+
 from agents.minimax.minimax import MinimaxAgent
+from agents.minimax.minimax_alpha_beta import MinimaxAlphaBetaAgent
+from agents.minimax.minimax_fixed_depth import MinimaxFixedDepthAgent
+from agents.minimax.minimax_alpha_beta_ordering import MinimaxAlphaBetaOrderingAgent
+from agents.minimax.minimax_iterative_deepening import MinimaxIterativeDeepeningAgent
+from agents.minimax.policies.chance.sample_refills import SampleRefillsPolicy
+from agents.minimax.policies.chance.round_termination import RoundTerminationPolicy
+from agents.minimax.policies.ordering.point_based import PointBasedPolicy
+from agents.minimax.policies.ordering.net_score_gain import NetScoreGainPolicy
+
 from agents.mcts.mcts import MonteCarloTreeSearchAgent
 from agents.mcts.mcts_reuse import MonteCarloTreeSearchReuseAgent
 from agents.mcts.mcts_chance import MonteCarloTreeSearchChanceAgent
@@ -9,25 +30,11 @@ from agents.mcts.mcts_rollout import MonteCarloTreeSearchRolloutAgent
 from agents.mcts.mcts_progressive_bias import MonteCarloTreeSearchProgressiveBiasAgent
 from agents.mcts.mcts_heuristic_expansion import MonteCarloTreeSearchHeuristicExpansionAgent
 from agents.mcts.mcts_progressive_widening import MonteCarloTreeSearchProgressiveWideningAgent
-from agents.minimax.minimax_alpha_beta import MinimaxAlphaBetaAgent
-from agents.minimax.minimax_fixed_depth import MinimaxFixedDepthAgent
-from agents.minimax.minimax_alpha_beta_ordering import MinimaxAlphaBetaOrderingAgent
-from agents.minimax.minimax_iterative_deepening import MinimaxIterativeDeepeningAgent
-from environments.azul.game import Game as AzulGame
-from environments.tic_tac_toe.game import Game as TicTacToeGame
-from interfaces.azul_console import AzulInputHandler
-from interfaces.tic_tac_toe_console import TicTacToeInputHandler
-from heuristics.azul_net_expected import AzulNetExpectedHeuristic
-from chance_policies.azul.round_termination import RoundTerminationPolicy
-from chance_policies.azul.sample_refills import SampleRefillsPolicy
-from chance_policies.azul.mcts_single_sample import SingleSamplePolicy
-from order_policies.azul.net_score_gain import NetScoreGainPolicy
-from order_policies.azul.point_based import PointBasedPolicy
-from rollout_policies.azul.point_epsilon import PointEpsilonPolicy
-from rollout_policies.azul.score_estimate_epsilon import ScoreEstimateEpsilonPolicy
-from expansion_policies.azul.point_based_random import PointBasedRandomExpansionPolicy
-from expansion_policies.azul.score_estimate_random import ScoreEstimateRandomExpansionPolicy
-from runner import Runner
+from agents.mcts.policies.chance.single_sample import SingleSamplePolicy
+from agents.mcts.policies.rollout.point_epsilon import PointEpsilonPolicy
+from agents.mcts.policies.rollout.score_estimate_epsilon import ScoreEstimateEpsilonPolicy
+from agents.mcts.policies.expansion.point_based_random import PointBasedRandomExpansionPolicy
+from agents.mcts.policies.expansion.score_estimate_random import ScoreEstimateRandomExpansionPolicy
 
 if __name__ == '__main__':
     game_seed = 42
