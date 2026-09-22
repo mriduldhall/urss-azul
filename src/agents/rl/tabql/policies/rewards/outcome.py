ee5594aafcdefada69106e94b997ebfb6315aa63
Mridul Dhall <mriduldhall1@gmail.com>
2026-09-22 15:39:26 +0100

    Build terminal outcome based reward

---
+++ src/agents/rl/tabql/policies/rewards/outcome.py
@@ -0,0 +1,12 @@
+class OutcomeReward:
+    @staticmethod
+    def get_reward(game, acting_player):
+        if not game.check_end():
+            return 0
+
+        victor = game.check_victory()
+        if victor is None:
+            return 0
+        elif victor == acting_player.value:
+            return 1
+        return -1
