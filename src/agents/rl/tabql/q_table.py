e71956cf3bae5bb62d7f67dd7c5f1ae0d8bb351d
Mridul Dhall <mriduldhall1@gmail.com>
2026-09-22 15:38:01 +0100

    Build q table class to store q values for states and actions

---
+++ src/agents/rl/tabql/q_table.py
@@ -0,0 +1,22 @@
+class QTable:
+    def __init__(self, action_count):
+        self.q_table = {}
+        self.action_count = action_count
+
+    def create_state(self, state):
+        if state not in self.q_table:
+            self.q_table[state] = [0.0] * self.action_count
+
+    def get_values(self, state):
+        self.create_state(state)
+        return self.q_table[state]
+
+    def get_value(self, state, action):
+        self.create_state(state)
+        return self.q_table[state][action]
+
+    def update(self, state, action, target, learning_rate):
+        self.create_state(state)
+        old_value = self.q_table[state][action]
+        self.q_table[state][action] = old_value + learning_rate * (target - old_value)
+        return self.q_table[state][action]
